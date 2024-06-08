b832a900f1233bea92c0d1707d8a3851e435dc95
ssr <ssrssr0095@gmail.com>
2024-06-08 18:01:10 +0530

    Update summarize.js

--- summarize.js
+++ summarize.js
@@ -18,7 +18,7 @@ async function summarizeText(text) {
     url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
     headers: { 
       'Content-Type': 'application/json', 
-      'Authorization': 'Bearer ' + process.env['SummarizeText']
+      'Authorization': 'Bearer ' + process.env.TextSummary_KEY
     },
     data : data
   };
